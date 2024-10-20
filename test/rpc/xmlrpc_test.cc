c0320928c1e9f325b2c344b3c1e952bb2aecc123
kannibalox <kannibalox@gmail.com>
2024-10-20 12:07:58 -0400

    Flesh out unit tests

--- test/rpc/xmlrpc_test.cc
+++ test/rpc/xmlrpc_test.cc
@@ -1,6 +1,5 @@
 #include "config.h"
 #include <string>
-#include <iostream>
 
 #include "command_helpers.h"
 #include "rpc/command_map.h"
@@ -10,12 +9,6 @@
 #include "globals.h"
 
 CPPUNIT_TEST_SUITE_REGISTRATION(XmlrpcTest);
-
-// #undef CMD2_A_FUNCTION
-
-// #define CMD2_A_FUNCTION(key, function, slot, parm, doc)      \
-//   m_map.insert_slot<rpc::command_base_is_type<rpc::function>::type>(key, slot, &rpc::function,   \
-//                     rpc::CommandMap::flag_dont_delete | rpc::CommandMap::flag_public_xmlrpc, NULL, NULL);
 
 torrent::Object xmlprpc_cmd_test_map_a(rpc::target_type t, const torrent::Object& obj) { return obj; }
 torrent::Object xmlprpc_cmd_test_map_b(rpc::target_type t, const torrent::Object& obj, uint64_t c) { return torrent::Object(c); }
@@ -32,7 +25,7 @@ XmlrpcTest::setUp() {
     cachedTime = rak::timer::current();
     control = new Control;
     //initialize_command_dynamic();
-    CMD2_ANY("test_a", &xmlprpc_cmd_test_map_a);
+    CMD2_ANY("xmlrpc_reflect", &xmlprpc_cmd_test_map_a);
     CMD2_ANY("test_b", std::bind(&xmlprpc_cmd_test_map_b, std::placeholders::_1, std::placeholders::_2, (uint64_t)2));
     CMD2_ANY_STRING("any_string", &xmlprpc_cmd_test_any_string);
 }
@@ -41,14 +34,18 @@ void
 XmlrpcTest::test_basics() {
   std::ifstream file; file.open("rpc/xmlrpc_test_data.txt");
   CPPUNIT_ASSERT(file.good());
+  std::vector<std::string> titles;
   std::vector<std::string> inputs;
   std::vector<std::string> outputs;
   std::string line;
   int index = 0;
   // Read file into inputs/outputs
   while (std::getline(file, line)) {
-    std::cout << "doc: " << index << " " << line << "\n";
+    if (line.size() == 0) {
+      continue;
+    }
     if (line[0] == '#') {
+      titles.push_back(line);
       continue;
     }
     if (index % 2) {
@@ -62,6 +59,6 @@ XmlrpcTest::test_basics() {
   for (int i = 0; i < inputs.size(); i++) {    
     auto output = std::string("");
     m_xmlrpc.process(inputs[i].c_str(), inputs[i].size(), [&output](const char* c, uint32_t l){ output.append(c, l); return true;});
-    CPPUNIT_ASSERT_EQUAL(std::string(outputs[i]), output);
+    CPPUNIT_ASSERT_EQUAL_MESSAGE(titles[i], std::string(outputs[i]), output);
   }
 }
